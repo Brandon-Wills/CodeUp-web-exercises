fe767552cd50e1729887fb9237e67bbcff041505
Brandon Wills <brandon.r.wills@gmail.com>
2021-02-04 17:25:34 -0600

    my funtionns work

---
+++ js/functions-lec.js
@@ -0,0 +1,76 @@
+//
+//
+//
+// function  someFunctionsNoParameters(){
+//     // inside of here we tell our function what to do
+//         return alert("Hello there Marco! ")
+// }
+// //someFunctionsNoParameters()
+//
+//
+// function someFunctionsNoParameters(name){
+//     //inside of here - we tell our function to do more stuff
+//     return alert("Hello There, " +name);
+//
+// }
+//
+// var myName = "Brandon";
+// someFunctionsNoParameters(myName)
+//
+// function increment(num){
+//
+//     return num + 1;
+//     }
+//     var four = increment(3);
+//     var five = increment(increment(3));
+//     var six = increment(increment(increment(3)));
+//
+//     console.log(four);
+//     console.log(five);
+//     console.log(six);
+//
+// console.log)(honkHorn());
+//
+// function multiplyByTwo(num){
+//     return num * 2;
+// }
+// console.log(multiplyByTwo(increment(3)));
+//
+// var honkHorn = function (){
+//    return " Beep Beep";
+// }
+//
+// console.log)(honkHorn());
+//
+// setting a default Parameter
+//
+// function sayHello(name = "Marco"){
+//     return alert( "Hello there, " +name);
+// }
+// sayHello(name : "Jake");
+//
+// var myVariable = "globalVar: Look I'm global for sure";
+//
+// function scopeExample({
+//     myVariable = "globalVar: I'm inside the function :)";
+//     var localVar = "localVar: look I'm Local";
+//     alert(localVar);
+//     alert(globalVar);
+//
+//  }
+// IIFE
+//
+// var iffeVar = "I'm out in the open, i hope nothing bad happens!"
+
+
+(function (){
+
+    var iffeVar = "globalVar: Look I'm global for sure";
+
+    console.log(iffeVar);
+
+    // Javascript Lives Here!
+
+
+})();
+
